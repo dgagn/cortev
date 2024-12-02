db0d3699eb82fb8cf4c98c34c46050757a7986d8
Dany Gagnon <admin@ovior.ca>
2024-12-02 00:29:39 -0500

    feat: remove the inner type for now

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use std::collections::HashMap;
+
+pub use session::Session;
+
 pub mod session;
 
 fn main() {
-    let session = session::Session::default();
-    let session = session.insert("key", 3);
-
-    println!("{:?}", session);
+    let session = Session::builder("woow")
+        .with_data(HashMap::new())
+        .build();
 }
--- src/session/mod.rs
+++ src/session/mod.rs
@@ -1,16 +1,73 @@
-mod inner;
+#![allow(dead_code)]
 
-type SessionKey = String;
+use std::collections::HashMap;
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+pub enum SessionState {
+    #[default]
+    Unchanged,
+    Changed,
+    Regenerated,
+    Invalidated,
+}
+
+#[derive(Debug)]
 pub struct Session {
-    inner: inner::Session,
-    key: SessionKey,
+    id: String,
+    state: SessionState,
+    data: HashMap<String, serde_json::Value>,
+}
+
+pub struct WithData;
+pub struct WithoutData;
+
+pub struct SessionBuilder<State = WithoutData> {
+    id: String,
+    data: Option<HashMap<String, serde_json::Value>>,
+    state: std::marker::PhantomData<State>,
+}
+
+impl SessionBuilder {
+    pub fn new(id: String) -> Self {
+        Self {
+            id,
+            data: None,
+            state: Default::default(),
+        }
+    }
+}
+
+impl SessionBuilder<WithoutData> {
+    pub fn with_data(self, data: HashMap<String, serde_json::Value>) -> SessionBuilder<WithData> {
+        SessionBuilder {
+            id: self.id,
+            data: Some(data),
+            state: std::marker::PhantomData::<WithData>,
+        }
+    }
+}
+
+impl SessionBuilder<WithData> {
+    pub fn build(self) -> Session {
+        Session {
+            id: self.id,
+            // Safe because `WithData` guarantees `data` is set
+            data: self.data.unwrap(),
+            state: SessionState::Unchanged,
+        }
+    }
 }
 
 impl Session {
-    pub fn key(&self) -> &str {
-        &self.key
+    pub fn builder<K>(id: K) -> SessionBuilder
+    where
+        K: Into<String>,
+    {
+        SessionBuilder::new(id.into())
+    }
+
+    pub fn id(&self) -> &str {
+        &self.id
     }
 
     pub fn get<K, V>(&self, key: K) -> Option<V>
@@ -18,7 +75,10 @@ impl Session {
         K: AsRef<str>,
         V: serde::de::DeserializeOwned,
     {
-        self.inner.get(key)
+        let key = key.as_ref();
+        self.data
+            .get(key)
+            .and_then(|value| serde_json::from_value(value.to_owned()).ok())
     }
 
     #[must_use]
@@ -27,27 +87,36 @@ impl Session {
         K: Into<String>,
         V: Into<serde_json::Value>,
     {
-        self.inner.insert(key, value.into());
+        let key = key.into();
+        self.data.insert(key, value.into());
+        self.state = SessionState::Changed;
         self
+    }
+
+    pub fn state(&self) -> SessionState {
+        self.state
     }
 
     #[must_use]
     pub fn regenerate(mut self) -> Self {
-        self.inner.regenerate();
+        self.state = SessionState::Regenerated;
         self
     }
 
     #[must_use]
     pub fn invalidate(mut self) -> Self {
-        self.inner.invalidate();
+        self.state = SessionState::Invalidated;
         self
     }
 
-    pub fn has(&self, key: &str) -> bool {
-        self.inner.has(key)
+    pub fn has<K>(&self, key: K) -> bool
+    where
+        K: AsRef<str>,
+    {
+        self.data.contains_key(key.as_ref())
     }
 
-    pub fn state(&self) -> inner::SessionState {
-        self.inner.state()
+    pub(crate) fn into_parts(self) -> (SessionState, HashMap<String, serde_json::Value>) {
+        (self.state, self.data)
     }
 }
