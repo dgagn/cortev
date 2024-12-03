a632d138a520c34779693661744088ffafd89b64
Dany Gagnon <admin@ovior.ca>
2024-12-02 23:27:35 -0500

    feat: test decryption

--- cortev-cookie/src/lib.rs
+++ cortev-cookie/src/lib.rs
@@ -109,4 +109,96 @@ mod tests {
         assert_eq!(typed_cookies[1].kind(), CookieKind::Signed);
         assert_eq!(typed_cookies[2].kind(), CookieKind::Normal);
     }
+
+    #[test]
+    fn test_cookies_from_request() {
+        let mut headers = HeaderMap::new();
+        headers.insert(
+            header::COOKIE,
+            "session=1234; csrftoken=5678; theme=light".parse().unwrap(),
+        );
+
+        let cookies: Vec<_> = cookies_from_request(&headers).collect();
+        assert_eq!(cookies.len(), 3);
+
+        assert_eq!(cookies[0].name(), "session");
+        assert_eq!(cookies[0].value(), "1234");
+
+        assert_eq!(cookies[1].name(), "csrftoken");
+        assert_eq!(cookies[1].value(), "5678");
+
+        assert_eq!(cookies[2].name(), "theme");
+        assert_eq!(cookies[2].value(), "light");
+    }
+
+    #[test]
+    fn test_cookie_jar() {
+        let key = cookie::Key::generate();
+        let policy = EncryptionCookiePolicy::default();
+        let jar = CookieJar {
+            jar: cookie::CookieJar::new(),
+            key,
+            encryption_policy: policy,
+        };
+
+        let mut headers = HeaderMap::new();
+        headers.insert(
+            header::COOKIE,
+            "session=1234; csrftoken=5678; theme=light".parse().unwrap(),
+        );
+
+        let jar = jar.from_headers(&headers);
+
+        let session = jar.jar.get("session").unwrap();
+        assert_eq!(session.value(), "1234");
+
+        let csrftoken = jar.jar.get("csrftoken").unwrap();
+        assert_eq!(csrftoken.value(), "5678");
+
+        let theme = jar.jar.get("theme").unwrap();
+        assert_eq!(theme.value(), "light");
+    }
+
+    #[test]
+    fn test_private_cookie_encrypted() {
+        let key = cookie::Key::generate();
+        let mut policy = EncryptionCookiePolicy::default();
+        policy.insert("id", CookieKind::Private);
+
+        let id = create_private_cookie_value(&key, "id", "1234");
+
+        let jar = CookieJar {
+            jar: cookie::CookieJar::new(),
+            key: key.clone(),
+            encryption_policy: policy,
+        };
+
+        let mut headers = HeaderMap::new();
+        headers.insert(
+            header::COOKIE,
+            format!("id={}; csrftoken=5678; theme=light", id)
+                .parse()
+                .unwrap(),
+        );
+
+        let jar = jar.from_headers(&headers);
+
+        let in_private = jar.jar.private(&key).get("id").unwrap();
+        let decrypt_cookie = jar.jar.private(&key).decrypt(in_private.clone()).unwrap();
+        assert_eq!(decrypt_cookie.value(), "1234");
+
+        let theme = jar.jar.get("theme").unwrap();
+        assert_eq!(theme.value(), "light");
+    }
+
+    fn create_private_cookie_value(
+        key: &cookie::Key,
+        id: &'static str,
+        value: &'static str,
+    ) -> String {
+        let mut id_encrypted = cookie::CookieJar::new();
+        let mut private_jar = id_encrypted.private_mut(key);
+        private_jar.add(Cookie::new(id, value));
+        id_encrypted.get(id).unwrap().value().to_owned()
+    }
 }
